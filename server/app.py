e6e3012bf2a760c43fceed211e09fb7958b37783
Anish <anish582006@gmail.com>
2026-02-10 14:41:17 +0530

    Setup done. Added virtual environment ad requirements.txt

---
+++ server/app.py
@@ -0,0 +1,9 @@
+from flask import Flask,jsonify
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+@app.route('/')
+def home():
+    return jsonify(message="Welcome to Tharun Railways!")
+if __name__=="__main__":
+    app.run(debug=True,port=5000)
