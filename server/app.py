67ae7aaa9204466fbed5e94a61b8ccd203fbc647
Rudra428 <neelamnihad1792@gmail.com>
2026-03-07 13:37:52 +0530

    backend part modified

--- server/app.py
+++ server/app.py
@@ -1,9 +1,287 @@
-from flask import Flask,jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import psycopg2
+import psycopg2.extras
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print(f"DEBUG: Password found is -> {os.getenv('DB_password')}")
 app=Flask(__name__)
 CORS(app)
-@app.route('/')
-def home():
-    return jsonify(message="Welcome to fintrack!")
+app.config['JWT_SECRET_KEY']='your-super-secret-jwt-key' 
+jwt=JWTManager(app)
+
+
+def db():
+    return psycopg2.connect(host="localhost",database="fintrack",user="postgres",password=os.getenv('DB_password'))
+
+
+
+#_____________________________________________________________________________________________________________________________________#
+#authentication section
+
+#register
+@app.route('/register', methods=['POST'])
+def register():
+    data=request.get_json()
+    email = data.get('email')
+    username=data.get('username')
+    password=data.get('password')
+    
+    if(not email or not username or not password):
+        return jsonify({'message':'Fill the required fields'}),400
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("select * from users where email=%s",(email,))
+        if cur.fetchone():
+            cur.close()
+            return jsonify({'message':'User already exist'}),400
+        hp=generate_password_hash(password)
+        cur.execute("insert into users (name,email,passhash) values (%s, %s, %s)",(username,email,hp))
+        conn.commit()
+        cur.close()
+        return jsonify({'message': 'User registered successfully!'}), 201
+    except Exception as e:
+        if conn: conn.rollback()
+        print("Error in database :",e)
+        return jsonify({'message':'Server error.Sorry for inconvenience caused!'}),500
+    finally:
+        if conn:conn.close()
+
+
+
+#login
+@app.route('/login', methods=['POST'])
+def login():
+    data=request.get_json()
+    email = data.get('email')
+    password=data.get('password')
+    if(not email or not password):
+        return jsonify({'message':'Fill the required fields'}),400
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("select user_id, email, passhash from users where email=%s",(email,))
+        user=cur.fetchone()
+        cur.close()
+        if user and check_password_hash(user['passhash'],password):
+            access_token=create_access_token(identity=user['user_id'])
+            return jsonify({'message':'Login successful.','access_token':access_token,'user_id':user['user_id']}),200
+        return jsonify({'message':'Invalid credentials.'}),401
+    except Exception as e:
+        print("Error in database :",e)
+        return jsonify({'message':'Server error.Sorry for inconvenience caused!'}),500
+    finally:
+        if conn:conn.close()
+
+@app.route('/protected',methods=['GET'])
+@jwt_required()
+def protected():
+    u=get_jwt_identity()
+    return jsonify({'message':f'Welcome,{u}!'}), 200
+
+
+
+#____________________________________________________________________________________________________________________#
+#accounts section
+
+
+@app.route('/accounts',methods=['POST'])
+def create_account():
+    data=request.get_json()
+    uid=data.get('user_id')
+    name=data.get('name')
+    bal=data.get('balance',0.00)
+
+    if not uid or not name:
+        return jsonify({'message':'user id and name are requireed'}),400
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor()
+        cur.execute("insert into accounts (user_id,name,balance) values (%s,%s,%s) returning account_id",(uid,name,bal))
+        acc_id=cur.fetchone()[0]
+        conn.commit()
+        return jsonify({'message':'Account created successfully','account_id':acc_id,'name':name,'balance':bal}),201
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        return jsonify({'message':str(e)}),500
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+@app.route('/accounts',methods=['GET'])
+def get_account():
+    uid=request.args.get('user_id')
+    if not uid:
+        return jsonify({'message':'user_id is required.'}),400
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor()
+        cur.execute("select account_id,name,balance from accounts where user_id=%s",(uid,))
+        acc=cur.fetchall()
+        acc_list=[]
+        for i in acc:
+            acc_list.append({
+                "account_id":i[0],"name":i[1],"balance":float(i[2])
+            })
+        return jsonify({'accounts':acc_list}),200
+    except Exception as e:
+        return jsonify({'message':str(e)}),500
+    finally:
+        if(conn):
+            cur.close()
+            conn.close()
+
+
+
+
+
+
+
+#___________________________________________________________________________________________#
+# categories section
+
+@app.route('/categories',methods=['GET'])
+def get_cat():
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor()
+        cur.execute("select category_id, name, is_essential from categories")
+        cat=cur.fetchall()
+        cat_list=[]
+        for i in cat:
+            cat_list.append({
+                'category_id':i[0],
+                'name':i[1],
+                'is_essential':i[2]
+            })
+        return jsonify({'category list':cat_list}),200
+    except Exception as e:
+        return jsonify({'message':str(e)}),500
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+
+@app.route('/categorywise_spending',methods=['GET'])
+def get_categorywise():
+    uid=request.args.get('user_id')
+
+    if not uid:
+        return jsonify({'message': 'user_id is required.'}), 400
+    conn=None
+    try:
+        conn=db()
+        cur=conn.cursor()
+        
+        cur.execute("""
+                        select c.name,SUM(t.amount)
+                    from transactions t,categories c
+                    where t.category_id=c.category_id and t.user_id=%s
+                    group by c.name
+                    order by SUM(t.amount) desc
+                    """,(uid,))
+        
+        sp=cur.fetchall()
+
+        sp_list=[]
+        for i in sp:
+            sp_list.append({
+                'category':i[0],
+                'spent_money':float(i[1])
+            })
+        return jsonify({'categorywise_spending':sp_list}),200
+    except Exception as e:
+        return jsonify({'message':str(e)}),500
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+
+#________________________________________________________________________________________________________#
+#transaction section
+
+@app.route('/transactions',methods=['POST'])
+def add_transaction():
+    data=request.get_json()
+    uid=data.get('user_id')
+    aid=data.get('account_id')
+    cid=data.get('category_id')
+    kharcha=data.get('amount')
+    
+    if not uid or not aid or not cid or kharcha is None:
+        return jsonify({'message': 'Missing required fields'}), 400
+    
+    conn=None
+    try:
+        conn = db()
+        cur = conn.cursor()
+        cur.execute("insert into transactions (user_id,account_id,category_id,amount) values (%s,%s,%s,%s) returning transaction_id",(uid,aid,cid,kharcha))
+        tid=cur.fetchone()[0]
+
+        cur.execute("update accounts set balance=balance-%s where account_id=%s and user_id=%s",(kharcha,aid,uid))
+        conn.commit()
+        return jsonify({'message': 'Transaction added successfully!', 'transaction_id': tid}), 201
+
+    except Exception as e:
+        return jsonify({'message':str(e)}),500
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+
+@app.route('/transactions',methods=['GET'])
+def det_transactions():
+    uid=request.args.get('user_id')
+    
+    if not uid:
+        return jsonify({'message': 'user_id is required.'}), 400
+    conn = None
+    try:
+        conn = db()
+        cur = conn.cursor()
+        cur.execute("""
+               select  t.transaction_id,t.amount,t.time_details,c.name,a.name
+               from transactions t, categories c,accounts a
+               where t.category_id=c.category_id and t.account_id=a.account_id and t.user_id=%s
+                order by t.time_details desc       
+        """,(uid,))
+        t=cur.fetchall()
+        t_list=[]
+        for i in t:
+            t_list.append({
+                'transaction_id':i[0],
+                'amount':float(i[1]),
+                'time_details':i[2].strftime("%Y-%m-%d %H:%M:%S"),
+                'category_name':i[3],
+                'account_name':i[4]
+            })
+        return jsonify({'transactions': t_list}), 200
+    except Exception as e:
+        return jsonify({'message':str(e)}),500
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True,port=5000)
--- server/init_db.py
+++ server/init_db.py
@@ -9,13 +9,27 @@ def setup_db():
         conn=psycopg2.connect(host="localhost",database="fintrack",user="postgres",password=os.getenv('DB_password'))
         cur=conn.cursor()
         cur.execute("drop table if exists transactions")
-        cur.execute("drop table if exists users")
         cur.execute("drop table if exists accounts")
         cur.execute("drop table if exists categories")
+        cur.execute("drop table if exists users")
+        categories = [
+            ('Food & Dining', True),
+            ('Rent/Mortgage', True),
+            ('Utilities', True),
+            ('Entertainment', False),
+            ('Shopping', False),
+            ('EMI/Loan', False),
+            ('Transportation', True),
+            ('Healthcare', True),
+            ('Others', False)
+        ]
+
         cur.execute("create table users(user_id serial primary key,name varchar(50),email varchar(100),passhash text)")
-        cur.execute("create table accounts(account_id serial primary key,name varchar(40),balance decimal(15,2))")
-        cur.execute("create table categories(category_id serial primary key,name varchar(20),is_essential bool)")
+        cur.execute("create table accounts(account_id serial primary key,user_id int,name varchar(40),balance decimal(15,2),foreign key(user_id) references users(user_id))")
+        cur.execute("create table categories(category_id serial primary key,name varchar(60),is_essential bool)")
         cur.execute("create table transactions(transaction_id serial primary key,user_id int,account_id int,category_id int,amount decimal(15,2),time_details timestamp default current_timestamp,foreign key(user_id) references users(user_id),foreign key(account_id) references accounts(account_id),foreign key(category_id) references categories(category_id))")
+        for i,j in categories:
+            cur.execute("insert into categories (name,is_essential) values (%s,%s)",(i,j))
         conn.commit()
         cur.close()
         conn.close()
