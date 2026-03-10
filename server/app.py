c9cb9c72fdd85ac877921397a2208613fdf69e75
Anish Kumar <anish582006@gmail.com>
2026-03-11 03:55:26 +0530

    Save me God

--- server/app.py
+++ server/app.py
@@ -18,10 +18,6 @@ def db():
    return psycopg2.connect(os.getenv('DATABASE_URL'),sslmode="require")
 
 @app.route("/initdb")
-def initdb():
-    from init_db import setup_db
-    setup_db()
-    return {"message": "database initialized"}
 #_____________________________________________________________________________________________________________________________________#
 #authentication section
 
